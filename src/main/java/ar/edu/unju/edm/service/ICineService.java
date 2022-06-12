8a786c9887f073c4556c7fde005499c3eac57a99
Usuario <Usuario@DESKTOP-QEN7T49>
2022-06-11 21:56:24 -0300

    Marcos Quinteros:Usuario terminado y Cine implementado en paquetes

--- src/main/java/ar/edu/unju/edm/service/ICineService.java
+++ src/main/java/ar/edu/unju/edm/service/ICineService.java
@@ -1,5 +1,15 @@
 package ar.edu.unju.edm.service;
 
-public interface ICineService {
+import java.util.List;
+import javax.validation.Valid;
+import org.springframework.stereotype.Service;
+import ar.edu.unju.edm.model.Cine;
 
+@Service
+public interface ICineService {
+	public List<Cine>mostrarCines();
+	public void eliminarCine(Integer idCine) throws Exception;
+	public void modificarCine(Cine cine);
+	public Cine buscarCine(Long idCine) throws Exception;
+	public void guardarCine(@Valid Cine cineparaguardar);
 }
