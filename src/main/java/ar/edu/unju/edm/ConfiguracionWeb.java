e84a1d1b6ba1173fe5e032818838feff9b170e57
Usuario <Usuario@DESKTOP-QEN7T49>
2022-07-04 13:36:49 -0300

    Marcos Quinteros: Crear usuario en el Login Terminado

--- src/main/java/ar/edu/unju/edm/ConfiguracionWeb.java
+++ src/main/java/ar/edu/unju/edm/ConfiguracionWeb.java
@@ -25,7 +25,7 @@ public class ConfiguracionWeb extends WebSecurityConfigurerAdapter {
 		http.csrf().disable()
 			.authorizeRequests()
 				.antMatchers(resources).permitAll()
-				.antMatchers("/", "/index").permitAll()
+				.antMatchers("/", "/index", "/otroUsuario").permitAll()
 				.anyRequest().authenticated()
 				.and()
 			.formLogin()
