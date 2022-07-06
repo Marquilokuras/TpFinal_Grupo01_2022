a5235f8d0ea061dd54aade5519fd9fa3e3d4ca31
Usuario <Usuario@DESKTOP-QEN7T49>
2022-07-06 19:27:44 -0300

    Marcos Quinteros: Correccion de errores, testeo de funcionalidad

--- src/main/java/ar/edu/unju/edm/model/UsuarioPelicula.java
+++ src/main/java/ar/edu/unju/edm/model/UsuarioPelicula.java
@@ -1,16 +1,15 @@
 package ar.edu.unju.edm.model;
 
 import java.time.LocalDate;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+//import javax.persistence.Table;
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Component;
